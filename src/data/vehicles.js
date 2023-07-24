676503374c4f1d0aec595583a713aeceb9cd1472
xtwgok3 <xtwgok@gmail.com>
2023-07-24 13:53:21 -0300

    sssss

--- src/data/vehicles.js
+++ src/data/vehicles.js
@@ -7,32 +7,37 @@ const vehicles = [
     VideoFrame: "https://cabrillos.github.io/tc/fl.html?get=LmN2YXR0di5jb20uYXIvbGl2ZS9jM2Vkcy9DYXJ0b29uTmV0d29yay9TQV9MaXZlX2Rhc2hfZW5jXzJBL0NhcnRvb25OZXR3b3JrLm1wZA==&key=ZDdkZjU2ZWZhYTM2YTcxMTQxZDIzODg1NGNjNDZkMWQ=&key2=OGFiYjJlZTkxNTBkOGIyYWY4ZWJlYzBkZTBmODMzYzg=&lang=0",
   },
   {
-    name: "Motorcycle",
-    description: "goes fast",
+    name: "HBO",
+    description: "HBO",
     image:
-      "https://content2.kawasaki.com/ContentStorage/KMC/Products/8798/8c85b065-f4e9-4ee6-966f-2b7993daff08.png?w=767",
+      "https://i.ibb.co/pLPD0WD/HBO.webp",
+    VideoFrame: "https://cabrillos.github.io/dtv/dtvc2.htm?get=Ly9kdHZvdHQtYWJjLmFrYW1haXplZC5uZXQvZGFzaF9saXZlXzExNDAvbWFuaWZlc3QubXBk&key=OTllZWZjMDY4ZGIzNTA5YjlmMGNmNWIzNDExNDIxOWQ=&key2=NmMyM2UzMWE0ZDgzYjU2OGU4NGFlYzYwOWE2M2ViYzk=",
   },
   {
-    name: "Bycicle",
-    description: "it's good for the environment",
+    name: "HBO PLUS",
+    description: "HBO PLUS",
     image:
-      "https://media.wired.com/photos/63e569c9de59d567d5d7c66d/master/w_2240,c_limit/Ride1Up-Cafe-Cruiser-Featured-Gear.jpg",
+      "https://i.ibb.co/jwh41qp/hboplus.png",
+    VideoFrame: "https://cabrillos.github.io/tc/fl.html?get=LmN2YXR0di5jb20uYXIvbGl2ZS9jNmVkcy9IQk9fUGx1cy9TQV9MaXZlX2Rhc2hfZW5jL0hCT19QbHVzLm1wZA==&key=ZjBlN2Y3ZDQ1ODk5MGVkZmFiN2I5OGI0MTI1NjQ2MTU=&key2=NzkyMDU3NTRiN2Y4NGE2MjY2MWMyZGJlOWRlNWRkNWQ=",
   },
   {
-    name: "Plane",
-    description: "Tickets are expensive",
-    image: "https://www.rd.com/wp-content/uploads/2020/01/GettyImages-1131335393-e1650030686687.jpg",
+    name: "Star-Channel",
+    description: "Star Channel",
+    image: "https://i.imgur.com/NYYkMbT.png",
+    VideoFrame: "https://cabrillos.github.io/tc/fl.html?get=LmN2YXR0di5jb20uYXIvbGl2ZS9jM2Vkcy9GT1hIRC9TQV9MaXZlX2Rhc2hfZW5jXzJBL0ZPWEhELm1wZA==&key=ODAyM2U4NWVmOWQxNzU2MWVlMWI0MjQxNzliMWIxNWU=&key2=NDc3YWQxODlmMWZjZDJiYWVjMDI2MDY4MTA3Zjg2MmY=",
   },
   {
-    name: "Ship",
-    description: "It is heavy",
-    image: "https://www.marineinsight.com/wp-content/uploads/2019/08/Cruise-ships-1.png",
+    name: "Warner BRos",
+    description: "Warner BRos",
+    image: "https://i.imgur.com/K1AFnWQ.png",
+    VideoFrame: "https://cabrillos.github.io/tc/fl.html?get=LmN2YXR0di5jb20uYXIvbGl2ZS9jN2Vkcy9XYXJuZXJIRC9TQV9MaXZlX2Rhc2hfZW5jXzJBL1dhcm5lckhELm1wZA==&key=MDY5YmQzZjBiNmMyNzk0NjdlMDg1NDlmMTdiZjViZDA=&key2=NWFmYTdlMzY5YTZkZTEwOTM4MThhODVhZjkxMmE3NzU=",
   },
   {
-    name: "Spaceship",
-    description: "Goes to space",
+    name: "History",
+    description: "History",
     image:
-      "https://img.freepik.com/premium-photo/futuristic-scifi-battle-spaceship-hover-ocean-alien-planet-digital-painting_379823-2914.jpg?w=2000",
+      "https://i.imgur.com/hL2YrRx.png",
+    VideoFrame: "https://cabrillos.github.io/tc/fl.html?get=LmN2YXR0di5jb20uYXIvbGl2ZS9jN2Vkcy9IaXN0b3J5SEQvU0FfTGl2ZV9kYXNoX2VuY18yQS9IaXN0b3J5SEQubXBk&key=ZTgyMzE4ZTUxOGJhNzBjZWEzZDdiMzdiZWY5OWU2OTI=&key2=YTA1ZmNiNjM0YzA3MWE1MTRlMzAzOWUxYzI3NGI0ZGI=",
   },
 ];
 
