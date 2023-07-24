6c82230c5c3ab41773d365854454f1133bdd8a47
xtwgok3 <xtwgok@gmail.com>
2023-07-24 12:42:09 -0300

    dddd

--- src/main.jsx
+++ src/main.jsx
@@ -2,34 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; // Utilizando BrowserRouter
 import vehicles from './data/vehicles.js';
 import VehicleView from './views/VehicleView.jsx';
 
-const routes = [
-  {
-    path: '/',
-    element: <App />,
-  },
-];
-
-vehicles.forEach((vehicle) => {
-  routes.push({
-    path: vehicle.name,
-    element: <VehicleView vehicle={vehicle} />,
-  });
-});
-
-// Ajusta el basename para que funcione en GitHub Pages
-const router = createBrowserRouter({ basename: '/vite-project/', routes });
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Agrega un elemento raíz aquí, por ejemplo, un div */}
-    <div>
-      <RouterProvider router={router}>
-        {/* También puedes poner otros componentes o elementos aquí si es necesario */}
-      </RouterProvider>
-    </div>
+    <Router basename="/vite-project/"> {/* Ajusta el basename para que funcione en GitHub Pages */}
+      <Route exact path="/" component={App} /> {/* Utiliza Route para renderizar el componente App */}
+      {vehicles.map((vehicle) => (
+        <Route
+          key={vehicle.name}
+          path={`/${vehicle.name}`}
+          render={() => <VehicleView vehicle={vehicle} />}
+        />
+      ))}
+    </Router>
   </React.StrictMode>
 );
