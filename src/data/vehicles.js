de0a0f9fecfd5d1e9a0b7ff152174c61a5e76ff8
xtwgok3 <xtwgok@gmail.com>
2023-08-19 13:15:43 -0300

    asd

--- src/data/vehicles.js
+++ src/data/vehicles.js
@@ -88,6 +88,20 @@ const vehicles = [
       "https://raw.githubusercontent.com/xtwgok3/vite-project/main/src/data/primal.png",
     VideoFrame: "https://cabrillos.github.io/mi-pagina/d4.html?id=4",
   },
+  {
+    name: "Anime-Xtrema",
+    description: "Anime-Xtrema",
+    image:
+      "https://i.imgur.com/fDlkOBx.png",
+    VideoFrame: "https://cabrillos.github.io/mi-pagina/d4.html?id=9",
+  },
+  {
+    name: "Anime-Zone-Tv",
+    description: "Anime-Zone-Tv",
+    image:
+      "https://i.imgur.com/yor5owS.png",
+    VideoFrame: "https://cabrillos.github.io/mi-pagina/d4.html?id=8",
+  },
 ];
 
 export default vehicles;
