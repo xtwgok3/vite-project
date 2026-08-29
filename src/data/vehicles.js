8ce6fc74b208aa9662d5077a7430a18b5a66fe2c
Carlos Alderete <124618617+xtwgok3@users.noreply.github.com>
2026-08-29 20:43:14 -0300

    Update vehicles.js

--- src/data/vehicles.js
+++ src/data/vehicles.js
@@ -129,6 +129,7 @@ const vehicles = [
     description: "MLs2aa",
     image: "https://cdn.freebiesupply.com/images/large/2x/mls-logo-black-and-white.png",
     VideoFrame: "https://la18hd.su/vivo/canal.php?stream=mls2en",
+    external: true,
   },
   {
     name: "MLS2",
